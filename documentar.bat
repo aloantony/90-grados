b3ca9fbfa1fc56d4a1a6886a6a5c4cd7b635b1fb
aloantony <142530308+aloantony@users.noreply.github.com>
2024-12-30 17:15:01 +0100

    ahora sí

---
+++ documentar.bat
@@ -0,0 +1,8 @@
+javadoc -author -version -private ^
+-encoding UTF-8 ^
+-docencoding UTF-8 ^
+-sourcepath .\src ^
+-d doc ^
+-link https://docs.oracle.com/en/java/javase/21/docs/api/ ^
+-overview overview.html ^
+-subpackages noventagrados
